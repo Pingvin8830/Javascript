68d42f875241bdf25fa145ddd0b5975e89a90a57
Vlasov Anton <Vlasov-Ant@yandex.ru>
2017-07-16 17:56:09 +0300

    random

---
+++ learn.javascript.ru/tasks/029-random_min_max.js
@@ -0,0 +1,10 @@
+/*
+  Напишите код для генерации случайного числа от min до max, не включая max.
+*/
+
+function random (min, max) {
+  return Math.random () * (max - min) + min;
+};
+
+alert (random (5, 7));
+
