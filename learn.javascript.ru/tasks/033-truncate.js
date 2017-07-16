efdd0a7be9693e000fc623c9b6b7d5bf8c45842e
Vlasov Anton <Vlasov-Ant@yandex.ru>
2017-07-16 18:37:12 +0300

    truncate

---
+++ learn.javascript.ru/tasks/033-truncate.js
@@ -0,0 +1,32 @@
+/*
+  Создайте функцию truncate (str, maxlength), которая проверяет длину строки str и, если она превосходит maxlength - заменяет конец str на '...' так,
+  чтобы её длина стала равна maxlength.
+
+  Результатом функции должна быть (при необходимости) усечённая строка.
+  Например:
+    truncate ('Вот, что мне хотелось бы сказать на эту тему:', 20) = 'Вот, что мне хоте...'
+    truncate ('Всем привет!', 20) = 'Всем привет!'
+*/
+
+function truncate (str, maxlength) {
+  if (str.length < maxlength) {
+    str = str;
+  } else {
+    str = str.slice (0, maxlength - 3) + '...';
+  };
+  return str
+};
+
+alert (truncate ('Вот, что мне хотелось бы сказать на эту тему:', 20));
+alert (truncate ('Всем привет!', 20));
+
+// Готовый вариант
+
+function truncate1 (str, maxlength) {
+  return (str.length > maxlength) ?
+    str.slice (0, maxlength - 3) + '...' :
+    str;
+};
+
+alert (truncate1 ('Вот, что мне хотелось бы сказать на эту тему:', 20));
+alert (truncate1 ('Всем привет!', 20));
