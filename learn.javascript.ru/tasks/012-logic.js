9486c18c7b22405401d786300cc03fb3891675e4
Vlasov Anton <Vlasov-Ant@yandex.ru>
2017-07-16 12:51:56 +0300

    Логика условностей

---
+++ learn.javascript.ru/tasks/012-logic.js
@@ -0,0 +1,25 @@
+/*
+  Напишите условие if для проверки того факта, что age НЕ находится между 14 и 90 включительно.
+  Сделайте два варианта условия: первый с использованием оператора НЕ !, второй - без этого оператора.
+*/
+
+var
+  age = +prompt ('Укажите возраст', 18),
+  min = 14,
+  max = 90,
+  isOutside = false;
+
+if (!(age >= 14 && age <= 90)) {
+  isOutside = true;
+};
+
+alert (isOutside);
+
+isOutside = false;
+
+if (age < 14 || age > 90) {
+  isOutside = true;
+};
+
+alert (isOutside);
+
