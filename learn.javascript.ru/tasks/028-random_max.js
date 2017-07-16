52ce616f63740b668a02e25dfa24374d07b5a1fc
Vlasov Anton <Vlasov-Ant@yandex.ru>
2017-07-16 17:52:18 +0300

    random

---
+++ learn.javascript.ru/tasks/028-random_max.js
@@ -0,0 +1,10 @@
+/*
+  Напишите код для генерации случайного значения в диапазоне от 0 до max, не включая max
+*/
+
+function random (max) {
+  return Math.random () * max;
+};
+
+alert (random (10));
+
