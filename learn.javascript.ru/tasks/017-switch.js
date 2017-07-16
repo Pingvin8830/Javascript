4bdb282e99b9ece0021acd63eb976a083a941eed
Vlasov Anton <Vlasov-Ant@yandex.ru>
2017-07-16 16:13:46 +0300

    Замена switch на if

---
+++ learn.javascript.ru/tasks/017-switch.js
@@ -0,0 +1,43 @@
+/*
+  Напишите if..else, соответствующий следующему switch:
+    switch (browser) {
+      case 'IE':
+        alert ('О, да у вас IE!');
+        break;
+
+      case 'Chrome':
+      case 'Firefox':
+      case 'Opera':
+        alert ('Да, и эти браузеры мы поддерживаем');
+        break;
+
+      default:
+        alert ('Мы надеемся, что и в вашем браузере все ок!');
+    };
+*/
+
+var browser = prompt ('Укажите браузер', 'Firefox');
+
+switch (browser) {
+  case 'IE':
+    alert ('О, да у вас IE!');
+    break;
+
+  case 'Chrome':
+  case 'Firefox':
+  case 'Opera':
+    alert ('Да, и эти браузеры мы поддерживаем');
+    break;
+
+  default:
+    alert ('Мы надеемся, что и в вашем браузере все ок!');
+};
+
+if (browser == 'IE') {
+  alert ('О, да у вас IE!');
+} else if (browser == 'Chrome' || browser == 'Firefox' || browser == 'Opera') {
+  alert ('Да, и эти браузеры мы поддерживаем');
+} else {
+  alert ('Мы надеемся, что и в вашем браузере все ок!');
+};
+
