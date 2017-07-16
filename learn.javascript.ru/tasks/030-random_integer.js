a8c20aa1d20fe28f4cb9f9cde439d2c8bc09a76f
Vlasov Anton <Vlasov-Ant@yandex.ru>
2017-07-16 18:09:17 +0300

    random

---
+++ learn.javascript.ru/tasks/030-random_integer.js
@@ -0,0 +1,13 @@
+/*
+  Напишите функцию randomInteger (min, max) для генерации случайного целого числа между min и max, включая min, max как возможные значения.
+  Любое число из интервала min..max должно иметь одинаковую вероятность.
+*/
+
+function randomInteger (min, max) {
+  var rand = Math.random ();
+  rand = min + rand * (max + 1 - min);
+  return Math.floor (rand);
+};
+
+alert (randomInteger (5, 12));
+
